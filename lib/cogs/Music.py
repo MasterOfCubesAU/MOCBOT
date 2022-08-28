aa46d17a441a38f5ae013c97f2a7f65fad01e615
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-08-28 23:45:01 +1000

    Initial commit

---
+++ lib/cogs/Music.py
@@ -0,0 +1,286 @@
+from ast import match_case
+from discord.ext import commands
+from discord import app_commands
+from lib.bot import config, logger, MOCBOT, DEV_GUILD, MOC_DB
+from typing import Literal, Union, Optional
+import discord
+
+from utils.Lavalink import LavalinkVoiceClient
+from utils.MusicFilters import FilterDropdownView, MusicFilters
+from utils.MusicQueue import QueueMenu, QueuePagination
+import lavalink
+import re
+import functools
+import asyncio
+import typing
+import datetime
+from functools import reduce
+
+class Music(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.players = {}
+
+        if not hasattr(bot, 'lavalink'):  # This ensures the client isn't overwritten during cog reloads.
+            bot.lavalink = lavalink.Client(bot.user.id)
+            bot.lavalink.add_node(config["LAVALINK"]["HOST"], config["LAVALINK"]["PORT"], config["LAVALINK"]["PASS"], 'eu', 'default-node')  # Host, Port, Password, Region, Name
+        
+        lavalink.add_event_hook(self.track_hook)
+        lavalink.add_event_hook(self.next_playing)
+        # lavalink.add_event_hook(self.progress_update)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info(f"[COG] Loaded {self.__class__.__name__}")
+        
+    async def cog_unload(self):
+        """ Cog unload handler. This removes any event hooks that were registered. """
+        self.bot.lavalink._event_hooks.clear()
+     
+    def interaction_ensure_voice(f):
+        @functools.wraps(f)
+        async def callback(self, interaction: discord.Interaction, *args, **kwargs) -> None:
+            await self.ensure_voice(await discord.ext.commands.Context.from_interaction(interaction))
+            await f(self, interaction, *args, **kwargs)
+        return callback
+
+    async def ensure_voice(self, ctx):
+        """ This check ensures that the bot and command author are in the same voice channel. """
+        if ctx.guild is None:
+            raise commands.CommandInvokeError('This command can only be used inside a Discord server.')
+        #  This is essentially the same as `@commands.guild_only()`
+        #  except it saves us repeating ourselves (and also a few lines)
+
+        player = self.bot.lavalink.player_manager.create(ctx.guild.id)
+        await player.set_volume(10)
+        # Create returns a player if one exists, otherwise creates.
+        # This line is important because it ensures that a player always exists for a guild.
+
+        # Most people might consider this a waste of resources for guilds that aren't playing, but this is
+        # the easiest and simplest way of ensuring players are created.
+
+        # These are commands that require the bot to join a voice channel (i.e. initiating playback).
+        # Commands such as volume/skip etc don't require the bot to be in a voice channel so don't need listing here.
+        should_connect = ctx.command.name in ('play')
+
+        if not ctx.author.voice or not ctx.author.voice.channel:
+            # Our cog_command_error handler catches this and sends it to the voice channel.
+            # Exceptions allow us to "short-circuit" command invocation via checks so the
+            # execution state of the command goes no further.
+            raise commands.CommandInvokeError('Join a voice channel first.')
+
+        v_client = ctx.voice_client
+        if not v_client:
+            if not should_connect:
+                raise commands.CommandInvokeError('Not connected.')
+
+            permissions = ctx.author.voice.channel.permissions_for(ctx.me)
+
+            if not permissions.connect or not permissions.speak:  # Check user limit too?
+                raise commands.CommandInvokeError('I need the `CONNECT` and `SPEAK` permissions.')
+
+            player.store('channel', ctx.channel.id)
+            await ctx.author.voice.channel.connect(cls=LavalinkVoiceClient)
+        else:
+            if v_client.channel.id != ctx.author.voice.channel.id:
+                raise commands.CommandInvokeError('You need to be in my voice channel.')
+
+    async def track_hook(self, event):
+        if isinstance(event, lavalink.events.QueueEndEvent):
+            # When this track_hook receives a "QueueEndEvent" from lavalink.py
+            # it indicates that there are no tracks left in the player's queue.
+            # To save on resources, we can tell the bot to disconnect from the voice channel.
+            guild_id = event.player.guild_id
+            guild = self.bot.get_guild(guild_id)
+            await guild.voice_client.disconnect(force=True)
+            await self.players[guild_id].delete()
+            del self.players[guild_id]
+    
+    async def next_playing(self, event):
+        if isinstance(event, lavalink.events.TrackStartEvent):
+            guild_id = event.player.guild_id
+            player = event.player
+            guild = self.bot.get_guild(guild_id)
+            logger.info(f"[MUSIC] [{guild} // {guild_id}] Playing {player.current.title} - {player.current.uri}")
+            if guild_id in self.players:
+                if player.current.stream:
+                    MusicFilters.clear_all(player)
+                embed = self.bot.create_embed("MOCBOT MUSIC", f"> NOW PLAYING: [{player.current.title}]({player.current.uri})", None)
+                embed.add_field(name="Duration",value=await self.formatDuration(player.current.duration) if not player.current.stream else "LIVE STREAM",inline=True)
+                embed.add_field(name="Uploader", value=player.current.author, inline=True)
+                embed.set_image(url=f"https://img.youtube.com/vi/{player.current.identifier}/maxresdefault.jpg")
+                embed.add_field(name="\u200b",value="**[LINK TO SOURCE]({})**".format(player.current.uri),inline=False)
+                embed.set_footer(text=f"Requested by {guild.get_member(player.current.requester)}")
+                await self.players[guild_id].edit(embed=embed)
+    
+    async def progress_update(self, event):
+        if isinstance(event, lavalink.events.PlayerUpdateEvent):
+            if event.position is not None:
+                print(await self.formatDuration(event.position))
+    
+    async def formatDuration(self, ms):
+        return datetime.datetime.utcfromtimestamp(ms / 1000).strftime("%Hh %Mm %Ss")
+       
+    @app_commands.command(name="play", description="Search and play media from YouTube, Spotify, SoundCloud etc.")
+    @app_commands.guilds(DEV_GUILD)
+    @app_commands.describe(
+        query="A search query or URL to the media."
+    )
+    @interaction_ensure_voice
+    async def play(self, interaction: discord.Interaction, query: str):
+        """ Searches and plays a song from a given query. """
+        await interaction.response.defer(thinking=True)
+        # Get the player for this guild from cache.
+        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+        # Remove leading and trailing <>. <> may be used to suppress embedding links in Discord.
+        query = query.strip('<>')
+
+        # Check if the user input might be a URL. If it isn't, we can Lavalink do a YouTube search for it instead.
+        # SoundCloud searching is possible by prefixing "scsearch:" instead.
+        if not re.compile(r'https?://(?:www\.)?.+').match(query):
+            query = f'ytsearch:{query}'
+
+        # Get the results for the query from Lavalink.
+        results = await player.node.get_tracks(query)
+
+        # Results could be None if Lavalink returns an invalid response (non-JSON/non-200 (OK)).
+        # ALternatively, results.tracks could be an empty array if the query yielded no tracks.
+        if not results or not results.tracks:
+            return await interaction.followup.send('No media matching your search query was found.')
+
+
+        # Valid loadTypes are:
+        #   TRACK_LOADED    - single video/direct URL)
+        #   PLAYLIST_LOADED - direct URL to playlist)
+        #   SEARCH_RESULT   - query prefixed with either ytsearch: or scsearch:.
+        #   NO_MATCHES      - query yielded no results
+        #   LOAD_FAILED     - most likely, the video encountered an exception during loading.
+        if results.load_type == 'PLAYLIST_LOADED':
+            tracks = results.tracks
+            for track in tracks:
+                # Add all of the tracks from the playlist to the queue.
+                player.add(requester=interaction.user.id, track=track)
+                logger.info(f"[MUSIC] [{interaction.guild} // {interaction.guild.id}] Queued {track.title} - {track.uri}")
+        else:
+            track = results.tracks[0]
+            player.add(requester=interaction.user.id, track=track)
+            logger.info(f"[MUSIC] [{interaction.guild} // {interaction.guild.id}] Queued {track.title} - {track.uri}")
+        
+        if player.current is not None:
+            embed = self.bot.create_embed("MOCBOT MUSIC", f"> ADDED TO QUEUE: [{player.queue[-1].title}]({player.queue[-1].uri})", None)
+            embed.set_image(url=f"https://img.youtube.com/vi/{player.queue[-1].identifier}/maxresdefault.jpg")
+            embed.add_field(name="POSITION",value=len(player.queue),inline=True)
+            embed.add_field(name="QUEUE TIME",value=await self.formatDuration(reduce(lambda a, b: a + b, [song.duration if not song.stream else 0 for song in player.queue])),inline=True)
+            embed.set_footer(text=f"Requested by {interaction.user}")
+            await interaction.followup.send(embed=embed)
+            await asyncio.sleep(10)
+            await interaction.delete_original_response()
+        else:
+            embed = self.bot.create_embed("MOCBOT MUSIC", f"> NOW PLAYING: [{player.queue[0].title}]({player.queue[0].uri})", None)
+            embed.add_field(name="Duration",value=await self.formatDuration(player.queue[0].duration) if not player.queue[0].stream else "LIVE STREAM",inline=True)
+            embed.add_field(name="Uploader", value=player.queue[0].author, inline=True)
+            embed.set_image(url=f"https://img.youtube.com/vi/{player.queue[0].identifier}/maxresdefault.jpg")
+            embed.add_field(name="\u200b",value="**[LINK TO SOURCE]({})**".format(player.queue[0].uri),inline=False)
+            embed.set_footer(text=f"Requested by {interaction.user}")
+            await interaction.followup.send(embed=embed)
+            self.players[interaction.guild.id] = await interaction.original_response()
+        
+        # We don't want to call .play() if the player is playing as that will effectively skip
+        # the current track.
+        if not player.is_playing:
+            await player.play()
+
+    @app_commands.command(name="skip", description="Skips the current media to the next one in queue.")
+    @app_commands.guilds(DEV_GUILD)
+    @app_commands.describe(
+        position="The queue item number to skip to."
+    )
+    @interaction_ensure_voice
+    async def skip(self, interaction: discord.Interaction, position: typing.Optional[int] = 1):
+        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+        if position < 1 or position > len(player.queue):
+            await interaction.response.send_message(embed=self.bot.create_embed("MOCBOT MUSIC", "You may only skip to a valid queue item.", None))
+            await asyncio.sleep(10)
+            return await interaction.delete_original_response()
+        player.queue = player.queue[position - 1:]
+        await player.play()
+        await interaction.response.send_message(embed=self.bot.create_embed("MOCBOT MUSIC", f"Successfully skipped to track [{player.current.title}]({player.current.uri}).", None))
+        await asyncio.sleep(10)
+        await interaction.delete_original_response()
+    
+    @app_commands.command(name="queue", description="Retrieve the music queue.")
+    @app_commands.guilds(DEV_GUILD)
+    async def queue(self, interaction: discord.Interaction):
+        pages = QueueMenu(source=QueuePagination(self.bot.lavalink.player_manager.get(interaction.guild.id), interaction=interaction, MusicCls=self), interaction=interaction)
+        await pages.start(await discord.ext.commands.Context.from_interaction(interaction))
+
+    @app_commands.command(name="seek", description="Seeks the current song.")
+    @app_commands.guilds(DEV_GUILD)
+    @app_commands.describe(
+        time="The time in seconds to seek to."
+    )
+    @interaction_ensure_voice
+    async def seek(self, interaction: discord.Interaction, time: int):
+        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+        if time < 0 or time > player.current.duration/1000:
+            return await interaction.response.send_message(embed=self.bot.create_embed("MOCBOT MUSIC", f"You may only seek between `0 and {player.current.duration/1000}` seconds.", None))
+        if not player.current.is_seekable:
+            await interaction.response.send_message(embed=self.bot.create_embed("MOCBOT MUSIC", f"This media does not support seeking.", None))
+            await asyncio.sleep(10)
+            return await interaction.delete_original_response()
+        await player.seek(time*1000)
+        await interaction.response.send_message(embed=self.bot.create_embed("MOCBOT MUSIC", f"Seeked to `{await self.formatDuration(time*1000)}`.", None))
+        await asyncio.sleep(10)
+        await interaction.delete_original_response()
+   
+    @app_commands.command(name="loop", description="Loop the current media or queue.")
+    @app_commands.guilds(DEV_GUILD)
+    @interaction_ensure_voice
+    async def loop(self, interaction: discord.Interaction, type: Literal["Song", "Queue (WIP)"]):
+        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+        match type:
+            case "Song":
+                player.set_repeat(not player.repeat)
+        await interaction.response.send_message(embed=self.bot.create_embed("MOCBOT MUSIC", f"{type} looping {'enabled' if player.repeat else 'disabled'}.", None))
+        await asyncio.sleep(10)
+        await interaction.delete_original_response()
+
+
+    @app_commands.command(name="disconnect", description="Disconnects the bot from voice.")
+    @app_commands.guilds(DEV_GUILD)
+    async def disconnect(self, interaction: discord.Interaction):
+        """ Disconnects the player from the voice channel and clears its queue. """
+        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+
+        if not interaction.guild.voice_client:
+            # We can't disconnect, if we're not connected.
+            return await interaction.response.send_message('Not connected.')
+
+        if not interaction.user.voice or (player.is_connected and  interaction.user.voice.channel.id != int(player.channel_id)):
+            # Abuse prevention. Users not in voice channels, or not in the same voice channel as the bot
+            # may not disconnect the bot.
+            return await interaction.response.send_message('You\'re not in my voice channel!')
+
+        # Clear the queue to ensure old tracks don't start playing
+        # when someone else queues something.
+        player.queue.clear()
+        # Stop the current track so Lavalink consumes less resources.
+        await player.stop()
+
+        if interaction.guild.id in self.players:
+            await self.players[interaction.guild.id].delete()
+            del self.players[interaction.guild.id]
+
+        # Disconnect from the voice channel.
+        await interaction.guild.voice_client.disconnect(force=True)
+        await interaction.response.send_message("MOCBOT Music has been stopped.", ephemeral=True)
+
+    @app_commands.command(name="filters", description="Toggles audio filters")
+    @app_commands.guilds(DEV_GUILD)
+    async def filters(self,  interaction: discord.Interaction):
+        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+        await interaction.response.send_message(view=FilterDropdownView(player))
+
+async def setup(bot):
+    await bot.add_cog(Music(bot))
---
+++ utils/MusicQueue.py
@@ -0,0 +1,97 @@
+from discord.ext import menus
+from discord.ui import Button, View
+import discord
+
+class QueueMenu(View, menus.MenuPages):
+    def __init__(self, source, interaction):
+        super().__init__(timeout=60)
+        self._source = source
+        self.current_page = 0
+        self.ctx = None
+        self.message = None
+        self.interaction = interaction
+        self.createButtons()
+
+    async def on_timeout(self) -> None:
+        await self.interaction.delete_original_response()
+
+    async def start(self, ctx, *, channel=None, wait=False):
+        # We wont be using wait/channel, you can implement them yourself. This is to match the MenuPages signature.
+        await self._source._prepare_once()
+        self.ctx = ctx
+        self.message = await self.send_initial_message(ctx, ctx.channel)
+
+    async def send_initial_message(self, ctx, channel):
+        page = await self._source.get_page(0)
+        kwargs = await self._get_kwargs_from_page(page)
+        await self.interaction.response.send_message(**kwargs)
+        return await self.interaction.original_response()
+
+    async def show_checked_page(self, page_number, interaction):
+        max_pages = self._source.get_max_pages()
+        try:
+            if max_pages is None:
+                # If it doesn't give maximum pages, it cannot be checked
+                await self.show_page(page_number, interaction)
+            elif max_pages > page_number >= 0:
+                await self.show_page(page_number, interaction)
+        except IndexError:
+            # An error happened that can be handled, so ignore it.
+            pass
+
+    async def show_page(self, page_number, interaction):
+        page = await self._source.get_page(page_number)
+        self.current_page = page_number
+        kwargs = await self._get_kwargs_from_page(page)
+        await interaction.response.edit_message(**kwargs)
+
+    async def _get_kwargs_from_page(self, page):
+        """This method calls ListPageSource.format_page class"""
+        value = await super()._get_kwargs_from_page(page)
+        if 'view' not in value:
+            value.update({'view': self})
+        return value
+
+    async def interaction_check(self, interaction):
+        """Only allow the author that invoke the command to be able to use the interaction"""
+        return interaction.user == self.ctx.author
+
+    def createButtons(self):
+        first_button = Button(label="First Page", style=discord.ButtonStyle.gray)
+        first_button.callback = self.first_page_callback
+        previous_button = Button(label="Previous Page", style=discord.ButtonStyle.gray)
+        previous_button.callback = self.previous_page_callback
+        next_button = Button(label="Next Page", style=discord.ButtonStyle.gray)
+        next_button.callback = self.next_page_callback
+        last_button = Button(label="Last Page", style=discord.ButtonStyle.gray)
+        last_button.callback = self.last_page_callback
+        self.add_item(first_button)
+        self.add_item(previous_button)
+        self.add_item(next_button)
+        self.add_item(last_button)
+
+    async def first_page_callback(self, interaction):
+        await self.show_page(0, interaction)
+    async def previous_page_callback(self, interaction):
+        await self.show_checked_page(self.current_page - 1, interaction)
+    async def next_page_callback(self, interaction):
+        await self.show_checked_page(self.current_page + 1, interaction)
+    async def last_page_callback(self, interaction):
+        await self.show_page(self._source.get_max_pages() - 1, interaction)
+
+
+class QueuePagination(menus.ListPageSource):
+    def __init__(self, player, interaction, MusicCls):
+        super().__init__(player.queue if player is not None else [], per_page=10)
+        self.interaction = interaction
+        self.Music = MusicCls
+        self.player = player
+        self.emptyQueueMsg = "Type `/play [SONG]` to add songs to the queue."
+
+    async def format_page(self, menu, entries):
+        offset = (menu.current_page * self.per_page) + 1
+        now_playing =  f"[{self.player.current.title}]({self.player.current.uri})" if self.player is not None else "N/A"
+        queueContent = "{}\n\n**CURRENT QUEUE:**\n{}".format(f"> NOW PLAYING: {now_playing}", "\n".join([f'{index}. [{track.title}]({track.uri}) - {await self.Music.formatDuration(track.duration) if not track.stream else "LIVE STREAM"}' for index, track in enumerate(entries, start=offset)]) if entries else self.emptyQueueMsg)
+        embed = self.interaction.client.create_embed("MOCBOT MUSIC", queueContent, None)
+        embed.set_footer(text=f"Page {menu.current_page + 1}/{self.get_max_pages() or 1} | Requested by {self.interaction.user}")
+        return embed
