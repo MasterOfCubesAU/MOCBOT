89e34caf267da2dd85506445c8ff7bdc66b8126f
MasterOfCubesAU <38149391+MasterOfCubesAU@users.noreply.github.com>
2022-11-06 12:34:45 +1100

    Create install.py
    Dependency installation script

---
+++ install.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+import shutil
+
+VENV_PATH = ".MOCBOT"
+
+def getBinPath():
+    if os.name == "posix":
+        return fr"{VENV_PATH}/bin"
+    else:
+        return f"{VENV_PATH}\Scripts"
+
+
+def main():
+    if args.reinstall or args.clean:
+        shutil.rmtree(VENV_PATH)
+        if args.clean:
+            return
+    if not os.path.exists(VENV_PATH):
+        os.system(f"python3.10 -m venv {VENV_PATH}")
+    os.system(f"{os.path.join(getBinPath(), 'pip')} install -r requirements.txt")
+    print("==================================================================================================")
+    print(fr"All dependencies have been installed/updated. To launch MOCBOT, execute {os.path.join(getBinPath(), 'python')} launcher.py")
+    print("==================================================================================================")
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Install all dependencies for MOCBOT.')
+    parser.add_argument('--reinstall', action='store_true', help='Force clean install.')
+    parser.add_argument('--clean', action='store_true', help='Removes all dependencies.')
+    args = parser.parse_args()
+    main()
