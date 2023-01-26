5cc34a42d52039a2073e550ff319fbd577e86635
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-01-26 18:52:37 +1100

    Added join role support

---
+++ lib/cogs/Roles.py
@@ -0,0 +1,40 @@
+from discord.ext import commands
+import logging
+from discord import Member, Object
+
+from utils.APIHandler import API
+from requests.exceptions import HTTPError
+
+class Roles(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger(__name__)
+
+    async def cog_load(self):
+        self.logger.info(f"[COG] Loaded {self.__class__.__name__}")
+
+    @staticmethod
+    async def give_join_roles(member: Member):
+        try:
+            rolesData = API.get(f'/roles/{member.guild.id}')
+        except HTTPError as e:
+            if e.response.status_code == 404:
+                return
+            else:
+                raise e 
+        if rolesData and rolesData.get("JoinRoles"):
+            for roleID in rolesData.get("JoinRoles"):
+                await member.add_roles(Object(id=int(roleID)), reason="Join Role")
+
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: Member):
+        settingsData = API.get(f'/settings/{member.guild.id}')
+        if "Verification" not in settingsData.get("EnabledModules"):
+            await Roles.give_join_roles(member)
+      
+
+
+async def setup(bot):
+    await bot.add_cog(Roles(bot))
