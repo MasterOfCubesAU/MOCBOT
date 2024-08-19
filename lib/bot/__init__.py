fe8baf241e714d9564d24623fa22c36691de06ef
sam1357 <67609409+sam1357@users.noreply.github.com>
2024-08-19 22:06:17 +1000

    👷 Add linting, CI and pre-commit hook (#87)
    Adding linting and a basic CI system, alognside a
    pre-commit hook. This is to maintain code
    readability and consistency across developer
    code.

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -6,67 +6,80 @@ import logging.config
 import logging
 import discord
 import os
+import typing
+
+from discord import Embed, Colour, Interaction, Message
 
 from lib.socket.Socket import Socket
 
+
 class MOCBOT(commands.Bot):
 
-    def __init__(self, is_dev):
+    def __init__(self, is_dev: bool) -> None:
         super().__init__(command_prefix="!", intents=discord.Intents.all())
         self.is_dev = is_dev
         self.mode = "DEVELOPMENT" if is_dev else "PRODUCTION"
         self.developers = API.get("/developers")
 
-    async def setup_hook(self):
+    async def setup_hook(self) -> None:
         self.setup_logger()
         await self.load_cog_manager()
         self.appinfo = await super().application_info()
         if self.appinfo.icon is not None:
             self.avatar_url = self.appinfo.icon.url
         else:
-            self.avatar_url = f"https://cdn.discordapp.com/embed/avatars/{int(self.user.discriminator) % 5}.png"
+            self.avatar_url = f"https://cdn.discordapp.com/embed/avatars/" f"{int(self.user.discriminator) % 5}.png"
 
-    def setup_logger(self):
+    def setup_logger(self) -> None:
         if not os.path.exists("logs"):
             os.makedirs("logs")
-            
+
         logging.config.dictConfig(Config.fetch()["LOGGING"])
         self.logger = logging.getLogger(__name__)
         for handler in logging.getLogger().handlers:
             if isinstance(handler, logging.FileHandler):
-                handler.encoding = 'utf-8'
-            if handler.name == "file" and os.path.isfile('logs/latest.log'):
+                handler.encoding = "utf-8"
+            if handler.name == "file" and os.path.isfile("logs/latest.log"):
                 handler.doRollover()
-        logging.getLogger('discord').setLevel(logging.DEBUG)
+        logging.getLogger("discord").setLevel(logging.DEBUG)
 
-    async def load_cog_manager(self):
+    async def load_cog_manager(self) -> None:
         await self.load_extension("lib.cogs.Cogs")
 
-    async def main(self):
+    async def main(self) -> None:
         await asyncio.gather(
-        super().start(Config.fetch()["TOKENS"][self.mode], reconnect=True),
-        Socket.start(self)
-    )
+            super().start(Config.fetch()["TOKENS"][self.mode], reconnect=True),
+            Socket.start(self),
+        )
 
-    def run(self):
+    def run(self) -> None:
         try:
             asyncio.run(self.main())
         except KeyboardInterrupt:
-            self.logger.info(f"MOCBOT is cleaning up processes.")
+            self.logger.info("MOCBOT is cleaning up processes.")
         finally:
-            self.logger.info(f"MOCBOT has been shut down gracefully.")
+            self.logger.info("MOCBOT has been shut down gracefully.")
 
-    def create_embed(self, title, description, colour=None):
-        embed = discord.Embed(title=None, description=description,
-                              colour=colour if colour else 0xDC3145, timestamp=discord.utils.utcnow())
-        embed.set_author(name=title if title else None,
-                         icon_url=self.avatar_url)
+    def create_embed(self, title: str, description: str, colour: typing.Union[Colour, int, None] = None) -> Embed:
+        embed = discord.Embed(
+            title=None,
+            description=description,
+            colour=colour if colour else 0xDC3145,
+            timestamp=discord.utils.utcnow(),
+        )
+        embed.set_author(name=title if title else None, icon_url=self.avatar_url)
         return embed
-    
-    async def is_developer(self, interaction):
+
+    async def is_developer(self, interaction: Interaction) -> bool:
         if interaction.user.id not in self.developers:
-            await interaction.response.send_message(embed=self.create_embed(
-                "MOCBOT SETUP", f"You must be a developer of the bot to be able to access this command.", None), ephemeral=True)
+            await interaction.response.send_message(
+                embed=self.bot.create_embed(
+                    "MOCBOT SETUP",
+                    "You must be a developer of the bot to be able to access this command.",
+                    None,
+                ),
+                ephemeral=True,
+            )
             return False
         return True
 
@@ -78,28 +91,30 @@ class MOCBOT(commands.Bot):
     #     async def extended_check(interaction):
     #         if interaction.guild is None:
     #             return False
-    #         return interaction.user.id in config.fetch()["DEVELOPERS"] or (interaction.user.id == 169402073404669952) or await original.predicate(interaction)
+    #         return interaction.user.id in config.fetch()["DEVELOPERS"] or
+    #         (interaction.user.id == 169402073404669952) or
+    #         await original.predicate(interaction)
     #     return app_commands.check(extended_check)
 
     # @staticmethod
     # def is_developer(interaction: discord.Interaction):
     #     return interaction.user.id in config.fetch()["Developers"]
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         self.appinfo = await super().application_info()
         self.avatar_url = self.appinfo.icon.url
-        self.logger.info(
-            f"Connected on {self.user.name} ({self.mode}) | d.py v{str(discord.__version__)}"
-        )
+        self.logger.info(f"Connected on {self.user.name} ({self.mode}) | d.py v{str(discord.__version__)}")
 
-    async def on_interaction(self, interaction):
+    async def on_interaction(self, interaction: Interaction) -> None:
         if interaction.type is discord.InteractionType.application_command:
             self.logger.info(
-                f"[COMMAND] [{interaction.guild} // {interaction.guild.id}] {interaction.user} ({interaction.user.id}) used command {interaction.command.name}")
+                f"[COMMAND] [{interaction.guild} // {interaction.guild.id}] {interaction.user} ({interaction.user.id})"
+                " used command {interaction.command.name}"
+            )
 
-    async def on_message(self, message):
+    async def on_message(self, message: Message) -> None:
         await self.wait_until_ready()
-        if (isinstance(message.channel, discord.DMChannel) and message.author.id in self.developers):
+        if isinstance(message.channel, discord.DMChannel) and message.author.id in self.developers:
             message_components = message.content.lower().split(" ")
             match message_components[0]:
                 case "sync":
--- lib/cogs/Guilds.py
+++ lib/cogs/Guilds.py
@@ -5,6 +5,7 @@ from utils.APIHandler import API
 import discord
 import logging
 
+
 class Guilds(commands.Cog):
 
     def __init__(self, bot):
@@ -13,17 +14,31 @@ class Guilds(commands.Cog):
 
     async def cog_load(self):
         self.logger.info(f"[COG] Loaded {self.__class__.__name__}")
-        
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        view=View()
-        view.add_item(discord.ui.Button(label="Setup",style=discord.ButtonStyle.link,url=f"https://mocbot.masterofcubesau.com/{guild.id}/manage"))
-        await guild.owner.send(embed=self.bot.create_embed("MOCBOT SETUP", f"To ensure full functionality of {self.bot.user.mention}, you must setup the bot to accomodate your server.", None), view=view)
-
+        view = View()
+        view.add_item(
+            discord.ui.Button(
+                label="Setup",
+                style=discord.ButtonStyle.link,
+                url=f"https://mocbot.masterofcubesau.com/{guild.id}/manage",
+            )
+        )
+        await guild.owner.send(
+            embed=self.bot.create_embed(
+                "MOCBOT SETUP",
+                f"To ensure full functionality of {self.bot.user.mention}, you must setup the bot to accomodate your "
+                "server.",
+                None,
+            ),
+            view=view,
+        )
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        API.delete(f'/settings/{guild.id}')
+        API.delete(f"/settings/{guild.id}")
+
 
 async def setup(bot):
     await bot.add_cog(Guilds(bot))
